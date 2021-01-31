86de392b66da993635a2f68f102dbf9fe6e49a44
Antonio <antoniomarcos997@gmail.com>
2021-01-30 23:30:54 -0300

    feat: connecto to mongodb before app listen

---
+++ src/main/config/env.ts
@@ -0,0 +1,4 @@
+export default {
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb+srv://admin:bwn8gO2RjYeUqMaz@nodecleanarch.oz8pf.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  port: process.env.PORT ?? 3000
+}
